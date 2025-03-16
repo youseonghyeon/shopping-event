458815ae74403d8207e4bc2888758a3e1c4e7957
유성현 <dolla_@naver.com>
2025-03-16 13:43:53 +0900

    feat: 단일 서버에서만 consumer를 사용하기 위해 @Conditional 처리로 consumer 변경

--- src/main/java/com/shop/shoppingevent/batch/GrantPointBatch.java
+++ src/main/java/com/shop/shoppingevent/batch/GrantPointBatch.java
@@ -5,12 +5,14 @@ import com.shop.shoppingevent.dto.EventPointMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
+@ConditionalOnProperty(name = "spring.kafka.enabled", havingValue = "true") // 단일 서버에서만 처리
 public class GrantPointBatch {
 
     private final ObjectMapper objectMapper;
